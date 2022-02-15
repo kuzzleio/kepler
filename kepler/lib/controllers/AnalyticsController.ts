cfb1dbb050992464d472fd68e3b73d5b38dcb9f6
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2022-02-15 11:59:27 +0100

    Get Country name from x-real-ip header if exists

--- kepler/lib/controllers/AnalyticsController.ts
+++ kepler/lib/controllers/AnalyticsController.ts
@@ -1,6 +1,7 @@
 import { Controller, KuzzleRequest } from "kuzzle";
 import Kepler from "../Kepler";
 import * as crypto from 'crypto';
+import fetch from 'node-fetch';
 
 export default class AnalyticsController extends Controller {
   public app: Kepler;
@@ -14,6 +15,21 @@ export default class AnalyticsController extends Controller {
           http: [{ verb: 'post', path: 'analytics/track' }]
         }
       }
+    }
+  }
+
+  private async getCountryFromIP(ip: string) {
+    try {
+      const response = await fetch(`https://api.iplocation.net?ip=${ip}`);
+      console.log(response)
+      if (! response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const { country_name } = await response.json();
+      return country_name === '' ? undefined : country_name;
+    } catch (error) {
+      this.app.log.error(`Failed to get country from IP: ${error}`);
     }
   }
 
@@ -32,6 +48,17 @@ export default class AnalyticsController extends Controller {
       user: crypto.createHash('sha256').update(user).digest('hex'), 
       version
     };
+
+    if (request.context.connection.misc.headers['x-real-ip']) {
+      const country = await Promise.race([
+        this.getCountryFromIP(request.context.connection.misc.headers['x-real-ip']), 
+        new Promise((resolve) => setTimeout(resolve, 1000))
+      ]);
+
+      if (country) {
+        trackingPayload.tags = { ...trackingPayload.tags, country }
+      }
+    }
 
     this.app.log.debug(`Analytics data reveived: ${JSON.stringify(trackingPayload)}`);
     const doc = await this.app.sdk.document.create(
