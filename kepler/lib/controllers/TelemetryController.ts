8c279ace69c2d218dae71c0f3d29fdd135dd4aa2
Adrien Maret <adrien@maret.email>
2022-02-23 11:18:27 +0100

    Use local geoip resolver (#5)
    * Update Kepler and Companion to provide faster response time
    
    * int
    
    * Use local geoip db
    
    * 1.1.3
    
    * 1.1.4

--- kepler/lib/controllers/TelemetryController.ts
+++ kepler/lib/controllers/TelemetryController.ts
@@ -1,7 +1,11 @@
-import { Controller, ExternalServiceError, KuzzleRequest } from "kuzzle";
-import Kepler from "../Kepler";
 import * as crypto from 'crypto';
-import fetch from 'node-fetch';
+
+import { Controller, KuzzleRequest } from 'kuzzle';
+import geoip from 'geoip-lite';
+
+import Kepler from "../Kepler";
+
+const IPV4_REGEX = /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/gm;
 
 export default class TelemetryController extends Controller {
   public app: Kepler;
@@ -15,20 +19,6 @@ export default class TelemetryController extends Controller {
           http: [{ verb: 'post', path: 'telemetry/register' }]
         }
       }
-    }
-  }
-
-  private async getCountryFromIP(ip: string) {
-    try {
-      const response = await fetch(`https://api.iplocation.net?ip=${ip}`);
-      if (! response.ok) {
-        throw new ExternalServiceError(response.statusText);
-      }
-
-      const { country_name } = await response.json();
-      return country_name === '' ? undefined : country_name;
-    } catch (error) {
-      this.app.log.error(`Failed to get country from IP: ${error}`);
     }
   }
 
@@ -57,23 +47,39 @@ export default class TelemetryController extends Controller {
         version
       };
 
-      if (request.context.connection.misc.headers['x-real-ip'] !== undefined) {
-        const country = await this.getCountryFromIP(request.context.connection.misc.headers['x-real-ip']);
+      const ip = this.getIp(request);
+      const result = geoip.lookup(ip);
 
-        if (country !== undefined) {
-          trackingPayload.tags = { ...trackingPayload.tags, country }
-        }
+      if (result) {
+        trackingPayload.tags.country = result.country;
+        trackingPayload.tags.city = result.city;
       }
 
       this.app.log.debug(`Analytics data reveived: ${JSON.stringify(trackingPayload)}`);
+
       const doc = await this.app.sdk.document.create(
         this.app.applicationConfig.analytics.index,
         this.app.applicationConfig.analytics.collection,
         trackingPayload);
+
       this.app.log.debug(`Analytics document created: ${JSON.stringify(doc)}`);
     }
     catch (error) {
       this.app.log.error(`Failed to save telemetry data: ${error}`);
     }
+  }
+
+  private getIp (request: KuzzleRequest) {
+    if (request.context.connection.misc.headers['x-real-ip']) {
+      return request.context.connection.misc.headers['x-real-ip'];
+    }
+
+    const ipv4 = request.context.connection.ips.find(ip => IPV4_REGEX.test(ip));
+
+    if (ipv4) {
+      return ipv4;
+    }
+
+    return request.context.connection.ips[0];
   }
 }
